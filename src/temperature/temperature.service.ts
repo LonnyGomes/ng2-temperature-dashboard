d924561df5af6e30f029e34be89cc28141518685
Lonny Gomes <code@lonnygomes.com>
2017-01-08 12:48:25 -0500

    Read in appSettings for temperature service

--- src/temperature/temperature.service.ts
+++ src/temperature/temperature.service.ts
@@ -2,21 +2,18 @@ import { Injectable, Component } from '@angular/core';
 import { TemperatureSensorComponent, ITemperatureSensor } from './temperature-sensor.component';
 import { Http } from '@angular/http';
 
+let settings = require('appSettings');
+
 @Injectable()
 export class TemperatureService {
     constructor(private http: Http) { }
 
-    hostName:string = 'localhost:3000';
+    apiHostName:string = settings.apiHostName;
     apiUrl:string = 'api/list/current/temperature';
-    deviceNames:string [] = [
-        'office',
-        'bedroom',
-        'outside',
-        'Living_Room_Thermostat'
-    ];
+    deviceNames:string [] = settings.deviceNames;
 
     getIndividualSensor(deviceName:string):Promise<ITemperatureSensor> {
-        return this.http.get(`http://${this.hostName}/${this.apiUrl}/${deviceName}`)
+        return this.http.get(`http://${this.apiHostName}/${this.apiUrl}/${deviceName}`)
             .toPromise()
             .then(response => {
                 let sensor:ITemperatureSensor = response.json().data as ITemperatureSensor;
@@ -26,6 +23,7 @@ export class TemperatureService {
     }
 
     getTemperatureSensors():Promise<ITemperatureSensor[]> {
+        console.log(settings);
         return Promise.all(this.deviceNames.map(curDeviceName => {
             return this.getIndividualSensor(curDeviceName);
         }));
