09a352cae77112849c80cfd9a9bbaa5e43fbeed1
Lonny Gomes <code@lonnygomes.com>
2017-01-08 22:37:59 -0500

    Added dew point calculations

--- src/temperature/temperature-sensor.component.ts
+++ src/temperature/temperature-sensor.component.ts
@@ -16,7 +16,31 @@ export interface ITemperatureSensor {
 export class TemperatureSensorComponent {
     @Input('sensorData') data:ITemperatureSensor;
 
-    get name():string {
+    private fahrenheitToCelsius(val:number) {
+        return (val - 32) * (5 / 9);
+    }
+
+    private celsiusToFahrenheit(val:number) {
+        return (val * (9 / 5)) + 32;
+    }
+
+    private calcDewPoint(temperature:number, humidity:number) {
+        //NOTE: the formula was taken from the following URL:
+        //      http://tinyurl.com/chtn2or
+        // T = temperature in fahrenheit degrees
+        // f = relative humidity
+        // Td = dew point temperature in celsius degrees
+        // TdF = dew point temperature in fahrenheit degrees
+        var T = this.fahrenheitToCelsius(temperature),
+            f = humidity,
+            Td = Math.pow((f / 100), (1 / 8)) * (112 + (0.9 * T)) + (0.1 * T) - 112,
+            TdF = this.celsiusToFahrenheit(Td);
+
+        //return the dew point rounded to 2 significant digits
+        return Math.round(this.celsiusToFahrenheit(Td) * 100) / 100;
+    }
+
+    get deviceName():string {
         return this.data.deviceName;
     }
 
@@ -26,5 +50,17 @@ export class TemperatureSensorComponent {
 
     get timeStamp():Date {
         return this.data.timeStamp;
+    }
+
+    get temperature():number {
+        return this.data.temperature;
+    }
+
+    get humidity():number {
+        return this.data.humidity;
+    }
+
+    get dewPoint():number {
+        return this.calcDewPoint(this.temperature, this.humidity);
     }
 }
