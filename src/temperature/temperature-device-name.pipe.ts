480091d7075c791ca6d79cc50f73f4ba378f2889
Lonny Gomes <code@lonnygomes.com>
2017-04-12 23:29:01 -0400

    Added maxlength value to device name
    If the length is exceeded, elipese is added

--- src/temperature/temperature-device-name.pipe.ts
+++ src/temperature/temperature-device-name.pipe.ts
@@ -1,5 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const maxLen = 12;
+
 @Pipe({
     name: 'temperatureDeviceName'
 })
@@ -7,9 +9,15 @@ export class TemperatureDeviceNamePipe implements PipeTransform {
     transform(value:string, args: string[]): any {
         if (!value) return value;
 
-        return value.replace(/\w\S*/g, function (txt) {
-            const subStr = txt.charAt(0).toUpperCase() + txt.substr(1);
-            return subStr.replace(/_/g, ' ');
+        return value.replace(/\w\S*/g, function (txt:string):string {
+            let subStr = txt.charAt(0).toUpperCase() + txt.substr(1);
+            subStr = subStr.replace(/_/g, ' ');
+
+            if (subStr.length < maxLen) {
+                return subStr;
+            } else {
+                return subStr.slice(0, maxLen - 3) + '...';
+            }
         });
     }
 }
